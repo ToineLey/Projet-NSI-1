c121496761d029805062b8b1188fcdf3af935c3e
ToineLey <144327016+ToineLey@users.noreply.github.com>
2023-10-03 08:50:23 +0200

    Update dates1.py

--- Tavail/modulaire/dates1.py
+++ Tavail/modulaire/dates1.py
@@ -11,19 +11,3 @@ def ajoute(s,x):
 def enumere():
     
     return
-
-
-def union(s1, s2):
-    s4=[]
-    for elem in s1:
-        if elem not in s2:
-            s4.append(elem)
-    return s4
-
-
-def intersection(s3, s4):
-    s5 = []
-    for elem in s3:
-        if elem in s4:
-            s5.append(elem)
-    return s5
