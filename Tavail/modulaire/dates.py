73dbabceba7b35d94c6840223efdcc0f01d2b68f
ToineLey <144327016+ToineLey@users.noreply.github.com>
2023-10-03 08:57:32 +0200

    Update dates.py

--- Tavail/modulaire/dates.py
+++ Tavail/modulaire/dates.py
@@ -7,22 +7,19 @@ def contient(s,x):
 def ajoute(s,x):
     return s.append(x)
 
-
-
-
-
-
 def union(s1, s2):
     s4=[]
-    for elem in s1:
+    for elem in range(len(s1)):
         if elem not in s2:
-            s4.append(elem)
+            s4.append(s1[elem])
+    for el in range(len(s2)):
+        s4.append(s2[el])
     return s4
 
 
 def intersection(s3, s4):
     s5 = []
-    for elem in s3:
-        if elem in s4:
-            s5.append(elem)
+    for elem in range(len(s3)):
+        if s3[elem] in s4:
+            s5.append(s3[elem])
     return s5
