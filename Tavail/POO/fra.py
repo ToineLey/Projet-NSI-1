91d5d5e50fe73c6c2f54d2b7ca48df4ba577a387
ToineLey <144327016+ToineLey@users.noreply.github.com>
2023-10-10 08:01:25 +0000

    mardi

---
+++ Tavail/POO/fra.py
@@ -0,0 +1,46 @@
+from divi import pgcd
+
+
+class Fraction:
+    def __init__(self, num, den):
+        self.numerateur = num
+        if den <= 0:
+            raise ValueError("Fraction avec dénominateur à 0")
+        else:
+            self.denominateur = den
+
+    def __str__(self):
+        n = str(self.numerateur)
+        if self.denominateur == 1:
+            d = ""
+        else:
+            d = " / " + str(self.denominateur)
+        return n + d
+
+    def __eq__(self, autre):
+        return (
+            self.numerateur * autre.denominateur
+            ==
+            self.denominateur * autre.numerateur
+        )
+
+    def __lt__(self, autre):
+        return (
+            self.numerateur * autre.denominateur
+            <
+            self.denominateur * autre.numerateur
+        )
+
+    def __add__(self, autre):
+        return Fraction(
+            self.numerateur*autre.denominateur+self.denominateur*autre.numerateur,
+            self.denominateur*autre.denominateur
+        )
+
+    def __mul__(self, autre):
+        return Fraction(
+            self.numerateur*autre.numerateur,
+            self.denominateur*autre.denominateur
+        )
+    def _irre(self):
+        return
---
+++ Tavail/POO/main.py
@@ -0,0 +1,4 @@
+from fra import Fraction
+a=Fraction(1,1)
+s=Fraction(3,1)
+print(a+s)
