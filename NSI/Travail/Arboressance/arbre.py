dd3fd2ddd5269691b52aa694591446b250daa843
ToineLey <toineleydert@gmail.com>
2023-11-30 12:06:51 +0100

    cours

--- NSI/Travail/Arboressance/arbre.py
+++ NSI/Travail/Arboressance/arbre.py
@@ -1,3 +1,7 @@
+import xml.dom.minidom as dom
+import os
+
+
 class Noeud:
     def __init__(self, v, f):
         self.valeur, self.fils = v, f
@@ -17,15 +21,40 @@ def parcour_prefixe(a):
     return ch
 
 
-def affiche(a):
-    ch =str(a.valeur)+"\n"
-    for f in a.fils:
-        ch+=affiche(f)
-        
-    return ch
+def affiche2(a, ch=""):
 
-def affiche2(a):    
-    ch =str(a.valeur)
-    print(ch+" ")
+    print(ch+str(a.valeur))
     for f in a.fils:
-        affiche2(f)
+        affiche2(f, " "+ch)
+
+
+"""def repertoire(r: str) -> Noeud:
+    assert os.path.isdir(r)
+    if not os.listdir(r)==[]:
+        return Noeud(r,[])
+    else:
+        return Noeud(r,)"""
+
+
+def repertoires(r: str) -> Noeud:
+    assert os.path.isdir(r)
+    fils = []
+    for elt in os.listdir(r):
+        if os.path.isdir(os.path.join(r, elt)):
+            fils.append(repertoires(os.path.join(r, elt)))
+        else:
+            fils.append(Noeud(os.path.join(r, elt), []))
+    return Noeud(r, fils)
+
+
+def compte_balise(d: dom, n) -> int:
+    if d.childNodes == []:
+        return 0
+    elif d.parentNode is None:
+        return compte_balise(d.childNode[0],n)
+    else:
+        for el in d.childNodes:
+            if d.nodeName == n:
+                return 1+compte_balise(el, n)
+            else:
+                return 0+compte_balise(el, n)
--- NSI/Travail/Arboressance/main.py
+++ NSI/Travail/Arboressance/main.py
@@ -6,5 +6,5 @@ a = Noeud("A", [Noeud("B", [Noeud("D", [])]),
                     Noeud("F", [Noeud("H", [])]),
                     Noeud("G", [])
                 ])])
-
-print(affiche2(a))
+domm=dom.parse("/home/tnsi-eleve5/projets/Projets-NSI/NSI/Travail/Arboressance/recette.xml")
+print(compte_balise(domm,"e"))
