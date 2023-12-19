e3f8621d90d8e4c997490e4f0688a743cb164811
ToineLey <toineleydert@gmail.com>
2023-12-19 10:06:48 +0100

    m

---
+++ NSI/Travail/knn/knn.py
@@ -0,0 +1,40 @@
+def hamming(a, b):
+    n = 0
+    if len(a) != len(b):
+        n += max(len(b), len(a))-min(len(b), len(a))
+        for i in range(min(len(b), len(a))):
+            if a[i] != b[i]:
+                n += 1
+    else:
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                n += 1
+
+    return n
+
+
+assert hamming('arbre', 'art') == 3
+assert hamming("si je ne m'abuse", "si je ne m'amuse") == 1
+assert hamming('canard', 'cannard') == 4
+assert hamming('amer', 'ramer') == 5
+
+
+# exo 2
+
+lexique = ['oui', 'non', 'yes', 'no', 'ok']
+
+def exii(m: str, d: int) -> None:
+    for trucs in lexique:
+        if hamming(trucs, m) <= d:
+            print(trucs)
+
+# exo 3
+            
+def exiii(m: str) -> None:
+    t=[]
+    for trucs in lexique:
+        t.append(hamming(trucs, m))
+    exii(m,min(t))
+    
+
+exiii('ok')
